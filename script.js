adacffaf446a8447842d4ddc61f90fe6aa582eff
gsilva <hello.gabrielsilva2@gmail.com>
2021-01-08 09:35:48 -0200

    transição de perguntas e botões de navegação

--- script.js
+++ script.js
@@ -1,20 +1,131 @@
-const btnInicio = document.querySelector(".inicio-btn");
-const btnFim = document.querySelector(".fim-btn");
-const pergunta = document.querySelector(".pergunta");
-const answers = document.querySelectorAll(".answer");
-let currentQuestion = 0;
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
+const
+    btnInicio = document.querySelector(".inicio-btn"),
+    btnFim = document.querySelector(".fim-btn"),
+    containerQuiz = document.querySelector(".container-quiz"),
+    respostas = document.querySelector(".respostas"),
+    pergunta = document.querySelector(".pergunta"),
+    answers = document.querySelectorAll(".answer"),
+    prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    questions = [
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing. 1",
+            answers: [
+                { text: "Lorem Ipsum is simply dummy text of the printing. 1", correct: true },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 1", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 1", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 1", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 1", correct: false}
+            ]
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing. 2",
+            answers: [
+                { text: "Lorem Ipsum is simply dummy text of the printing. 2", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 2", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 2", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 2", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 2", correct: true }
+            ]
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing. 3",
+            answers: [
+                { text: "Lorem Ipsum is simply dummy text of the printing. 3", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 3", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 3", correct: true },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 3", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 3", correct: false }
+            ]
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing. 4",
+            answers: [
+                { text: "Lorem Ipsum is simply dummy text of the printing. 4", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 4", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 4", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 4", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 4", correct: true }
+            ]
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing. 5",
+            answers: [
+                { text: "Lorem Ipsum is simply dummy text of the printing. 5", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 5", correct: true },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 5", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 5", correct: false },
+                { text: "Lorem Ipsum is simply dummy text of the printing. 5", correct: false }
+            ]
+        }
+    ];
 
-const questions = [
-    {
-        question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        answers: [
-            { text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", correct: true },
-            { text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", correct: false },
-            { text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", correct: false },
-            { text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", correct: false },
-            { text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", correct: false}
-        ]
+let currentQuestion = 0;
+
+window.addEventListener("DOMContentLoaded", () => {
+    showQuestion();
+});
+
+btnInicio.addEventListener("click", startQuiz);
+
+next.addEventListener("click", () => {
+
+    currentQuestion++;
+
+    showQuestion();
+    verifyIndexQuestions();
+});
+
+prev.addEventListener("click", () => {
+
+    currentQuestion--;
+
+    showQuestion();
+    verifyIndexQuestions();
+});
+
+function startQuiz() {
+    btnInicio.classList.add("disabled");
+    containerQuiz.classList.add("enabled");
+}
+
+function showQuestion() {
+
+    let item = questions[currentQuestion];
+    pergunta.textContent = item.question;
+
+    let answersQuestions = item.answers.map((answer, index) => {
+        
+        return `
+            <fieldset>
+                <input id="res${index + 1}" type="radio" name="input">
+                <label class="answer" for="res${index + 1}">${answer.text}</label>
+            </fieldset>
+        `;
+
+    }).join("");
+    respostas.innerHTML = answersQuestions;
+
+}
+
+function verifyIndexQuestions() {
+
+    if(currentQuestion === 4) {
+        next.classList.add("remove");
+        btnFim.classList.remove("remove");
+    } else {
+        next.classList.remove("remove");
+        btnFim.classList.add("remove");
     }
-]
+
+    if(currentQuestion === 0) {
+        prev.classList.add("remove");
+    } else {
+        prev.classList.remove("remove");
+    }
+
+}
+
+function getCorrectAnswers() {
+    console.log("IHAAAA!");
+}
